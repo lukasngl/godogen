f979d56aa1da73ad48eaeed49ca72d10eee46529
Lukas Nagel <lukasnagel@fastmail.com>
2025-10-16 19:23:45 +0200

    feat(lsp): goto definition for steps

--- godogen-language-server/init.lua
+++ godogen-language-server/init.lua
@@ -1,13 +1,15 @@
-vim.lsp.config["luals"] = {
+vim.lsp.config["godogen-language-server"] = {
 	-- Command and arguments to start the server.
-	cmd = { "go run ." },
+	cmd = { "go", "run", "github.com/lukasngl/godogen-language-server" },
 	-- Filetypes to automatically attach to.
 	filetypes = { "go", "cucumber" },
 	-- Sets the "workspace" to the directory where any of these files is found.
 	-- Files that share a root directory will reuse the LSP server connection.
 	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
-	root_markers = { { "go.mod", ".git" } },
+	root_markers = { "go.mod", ".git" },
 	-- Specific settings to send to the server. The schema is server-defined.
 	-- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
 	settings = {},
 }
+
+vim.lsp.enable("godogen-language-server")
